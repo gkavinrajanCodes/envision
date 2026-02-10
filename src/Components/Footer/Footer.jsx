5b32f37255c60f711b4ce0e59489082b6f9936ed
Pranav P <pranavkrishnap856@gmail.com>
2026-02-10 14:10:56 +0530

    fix credits in footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className='footer'>
         <p>&copy; 2026 Envision IEEECS. All rights reserved.</p>
-        <p>Created and maintained by G. Kavin Rajan, Technical Team Member, IEEECS SSN</p>
+        <p>Created and maintained by Technical Team, IEEECS SSN</p>
     </div>
   )
 }
