024697ba3d91353627c425e64d7fa142631a7a53
Pranav P <pranavkrishnap856@gmail.com>
2026-02-11 20:01:23 +0530

    update organisers

--- src/Components/Sponsors/Sponsors.jsx
+++ src/Components/Sponsors/Sponsors.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import './Sponsors.css'
-import sponsor1 from '../../assets/ieeecs-ssn-dark.png'
-import sponsor2 from '../../assets/iic.png'
-import sponsor3 from '../../assets/SSN-01.png'
-
+import sponsor1 from '../../assets/SSN-01.png'
+import sponsor2 from '../../assets/ieeecs-ssn-dark.png'
+import sponsor3 from '../../assets/gfg-ssn-body.png'
+import sponsor4 from '../../assets/iic.png'
 
 const Sponsors = () => {
   return (
     <div className='sponsor'>
         <div className="sponsor-logos">
-            <img src={sponsor3} alt="Sponsor 1" />
-            <img src={sponsor1} alt="Sponsor 2" />
-            <img src={sponsor2} alt="Sponsor 3" />
+            <img src={sponsor1} alt="SSN" />
+            <img src={sponsor2} alt="IEEECS SSN" />
+            <img src={sponsor3} alt="GFG SSN" />
+            <img src={sponsor4} alt="IIC" />
         </div>
     </div>
   )
