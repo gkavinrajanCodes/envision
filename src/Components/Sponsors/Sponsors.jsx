400c9270cea2e8a80a1bf1fc4e4187c4fc111c70
Pranav P <pranavkrishnap856@gmail.com>
2026-02-12 11:00:29 +0530

    replace ssn logo file

--- src/Components/Sponsors/Sponsors.jsx
+++ src/Components/Sponsors/Sponsors.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Sponsors.css'
-import sponsor1 from '../../assets/SSN-01.png'
+import sponsor1 from '../../assets/ssn.png'
 import sponsor2 from '../../assets/ieeecs-ssn-dark.png'
 import sponsor3 from '../../assets/gfg-ssn-body.png'
 import sponsor4 from '../../assets/iic.png'
