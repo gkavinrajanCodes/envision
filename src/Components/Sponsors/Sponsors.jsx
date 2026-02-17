bdcdab5f0008ef800dc71237e4fdf3aac20375dc
Pranav P <pranavkrishnap856@gmail.com>
2026-02-17 10:16:33 +0530

    add ieeecsmas logo (and fixed mobile layout)

--- src/Components/Sponsors/Sponsors.jsx
+++ src/Components/Sponsors/Sponsors.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import './Sponsors.css'
 import sponsor1 from '../../assets/ssn.png'
-import sponsor2 from '../../assets/ieeecs-ssn-dark.png'
-import sponsor3 from '../../assets/ieeemas.png'
-import sponsor4 from '../../assets/gfg-ssn-body.png'
-import sponsor5 from '../../assets/iic.png'
+import sponsor2 from '../../assets/ieeemas.png'
+import sponsor3 from '../../assets/ieeecsmas.png'
+import sponsor4 from '../../assets/iic.png'
+import sponsor5 from '../../assets/ieeecs-ssn-dark.png'
+import sponsor6 from '../../assets/gfg-ssn-body.png'
 
 const Sponsors = () => {
   return (
     <div className='sponsor'>
         <div className="sponsor-logos">
             <img src={sponsor1} alt="SSN" />
-            <img src={sponsor2} alt="IEEECS SSN" />
-            <img src={sponsor3} alt="IEEE MAS" />
-            <img src={sponsor4} alt="GFG SSN" />
-            <img src={sponsor5} alt="IIC" />
+            <img src={sponsor2} alt="IEEE MAS" />
+            <img src={sponsor3} alt="IEEE CS MAS" />
+            <img src={sponsor4} alt="IIC" />
+            <img src={sponsor5} alt="IEEE CS SSN" />
+            <img src={sponsor6} alt="GFG SSN" />
         </div>
     </div>
   )
